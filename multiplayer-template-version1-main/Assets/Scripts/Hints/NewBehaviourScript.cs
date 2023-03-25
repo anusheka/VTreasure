76262880964a13e9a6e44bca8121c6e5e1458537
nikbuckwalter <nikbuckwalter@vt.edu>
2023-03-25 15:18:54 -0400

    Added the hints scene and the script to to display the hint text

---
+++ multiplayer-template-version1-main/Assets/Scripts/Hints/NewBehaviourScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Text Textfield;
+
+    public void SetText(string text)
+    {
+        Textfield.text = text;
+    }
+}
